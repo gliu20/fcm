4ef9c454a24cbfbdd3bc91155e83a23297640853
gliu20 <gliu10000@gmail.com>
2020-03-08 15:14:39 -0400

    move story data to App.js

--- App.js
+++ App.js
@@ -4,19 +4,28 @@ import HomeScreen from './screens/homescreen';
 import BadgeScreen from './screens/badgescreen';
 import StoryScreen from './screens/storyscreen'
 
+const story = [
+    { key: "1", storyImage: require('./assets/back.jpg'), storyText: "One day there was a" },
+    { key: "2", storyImage: require('./assets/back.jpg'), storyText: "the next day" },
+    { key: "3", storyImage: require('./assets/back.jpg'), storyText: "blah blah" },
+    { key: "4", storyImage: require('./assets/back.jpg'), storyText: "and something something" },
+    { key: "5", storyImage: require('./assets/back.jpg'), storyText: "slide 5" },
+    { key: "6", storyImage: require('./assets/splash.png'), storyText: "slide 6" }
+]
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <StoryScreen></StoryScreen>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <StoryScreen story={story}></StoryScreen>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 25,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        marginTop: 25,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
--- screens/storyscreen.js
+++ screens/storyscreen.js
@@ -2,19 +2,10 @@ import React from 'react';
 import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
 import Story from './../components/story';
 
-const data = [
-    {key:"1",storyImage: require('../assets/back.jpg'), storyText:"One day there was a"},    
-    {key:"2",storyImage: require('../assets/back.jpg'), storyText:"the next day"},
-    {key:"3",storyImage: require('../assets/back.jpg'), storyText:"blah blah"},
-    {key:"4",storyImage: require('../assets/back.jpg'), storyText:"and something something"},
-    {key:"5",storyImage: require('../assets/back.jpg'), storyText:"slide 5"},
-    {key:"6",storyImage: require('../assets/splash.png'), storyText:"slide 6"}    
-]
-
 class StoryScreen extends React.Component {
   render () {
     return (
-        <FlatList data={data} horizontal={true} pagingEnabled={true} style={styles.container} renderItem={({item}) => <Story storyImage={item.storyImage} storyText={item.storyText}></Story>}>
+        <FlatList data={this.props.story} horizontal={true} pagingEnabled={true} style={styles.container} renderItem={({item}) => <Story storyImage={item.storyImage} storyText={item.storyText}></Story>}>
         </FlatList>
     );
   }
