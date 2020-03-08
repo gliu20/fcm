efec1dd082cf66d5d6b5dadf97f9917cda9dcc76
gliu20 <gliu10000@gmail.com>
2020-03-08 15:12:12 -0400

    refactor out data from story

--- screens/storyscreen.js
+++ screens/storyscreen.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
 import Story from './../components/story';
+
+const data = [
+    {key:"1",storyImage: require('../assets/back.jpg'), storyText:"One day there was a"},    
+    {key:"2",storyImage: require('../assets/back.jpg'), storyText:"the next day"},
+    {key:"3",storyImage: require('../assets/back.jpg'), storyText:"blah blah"},
+    {key:"4",storyImage: require('../assets/back.jpg'), storyText:"and something something"},
+    {key:"5",storyImage: require('../assets/back.jpg'), storyText:"slide 5"},
+    {key:"6",storyImage: require('../assets/splash.png'), storyText:"slide 6"}    
+]
 
 class StoryScreen extends React.Component {
   render () {
     return (
-        <ScrollView horizontal={true} pagingEnabled={true} style={styles.container}>
-            <Story storyImage={require('../assets/back.jpg')} storyText="One day there was a"></Story>
-            <Story storyImage={require('../assets/back.jpg')} storyText="the next day"></Story>
-            <Story storyImage={require('../assets/back.jpg')} storyText="blah blah"></Story>
-            <Story storyImage={require('../assets/back.jpg')} storyText="and something something"></Story>
-            <Story storyImage={require('../assets/back.jpg')} storyText="slide 5"></Story>
-            <Story storyImage={require('../assets/splash.png')} storyText="slide 6"></Story>
-        </ScrollView>
+        <FlatList data={data} horizontal={true} pagingEnabled={true} style={styles.container} renderItem={({item}) => <Story storyImage={item.storyImage} storyText={item.storyText}></Story>}>
+        </FlatList>
     );
   }
 }
